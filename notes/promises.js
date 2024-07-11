bb2e1b7784a6041dc12225d4a26f64bfec960e42
tarunvoora-kore <tarun.voora@kore.com>
2024-07-11 16:36:50 +0530

    nodepractice files

--- notes/promises.js
+++ notes/promises.js
@@ -22,4 +22,19 @@ async function executeTask() {
 }
 
 
-executeTask()
+// executeTask()
+
+
+const fs = require('fs').promises;
+
+ async function readFile() {
+  try {
+    const data =  await fs.readFile('example.txt', 'utf8');
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+readFile();
+console.log('Reading file...');
